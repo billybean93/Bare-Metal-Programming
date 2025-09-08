d43968e920471ff89a6dc3e9d7b979516f0d2ad0
BillyBean <alexandernguyenbill@gmail.com>
2025-09-08 17:45:59 +0700

    - add drawFinishLine - add comments

--- Task 3/map/pipe.c
+++ Task 3/map/pipe.c
@@ -2,6 +2,8 @@
 #include "../kernel/framebf.h"
 #include "../uart/uart1.h"
 
+// Simple linear congruential generator for pseudo-random numbers
+
 unsigned int random(unsigned int N) {
     register unsigned long t;
    
@@ -10,6 +12,8 @@ unsigned int random(unsigned int N) {
  
     return 70 + (t % (N + 1)) ; //70 - N
 }
+
+// Function to draw a single pipe (top and bottom)
 
 void drawPipe(Pipe p, int bufferIndex) {
 
@@ -31,6 +35,7 @@ void drawPipe(Pipe p, int bufferIndex) {
 
    
 }
+// Function to initialize pipes with random gap positions
 
 void initPipes(Pipe pipes[], int num_pipes, unsigned int max, unsigned int min) {
     for (int i = 0; i < num_pipes; i++) {
@@ -44,6 +49,7 @@ void initPipes(Pipe pipes[], int num_pipes, unsigned int max, unsigned int min) 
         
     }
 }
+// Function to draw all pipes
 
 void drawPipes(Pipe pipes[], int num_pipes, int bufferIndex) {
     for (int i = 0; i < num_pipes; i++) {
@@ -51,12 +57,15 @@ void drawPipes(Pipe pipes[], int num_pipes, int bufferIndex) {
     }
 }
 
+// Function to update pipe positions
+
 
 void updatePipes(Pipe pipes[], int num_pipes) {
     for (int i = 0; i < num_pipes; i++) {
         pipes[i].x -= PIPE_SPEED;
     }
 }
+// Function to draw the background
 
 void drawBackground(int bufferIndex) {
     if (bufferIndex) {
@@ -70,6 +79,7 @@ void drawBackground(int bufferIndex) {
     
 
 }
+// Function to draw the entire map (background + pipes)
 
 void drawMap(Pipe pipes[], int num_pipes, int bufferIndex) {
     if (bufferIndex) {
@@ -83,6 +93,38 @@ void drawMap(Pipe pipes[], int num_pipes, int bufferIndex) {
     }
 }
 
+// Function to draw the finish line with a checkerboard pattern
+
+void drawFinishLine(int x) {
+    int y = 0;
+    int maxHeight = BACKGROUND_IMAGE_HEIGHT - GROUND_HEIGHT;
+
+    // Loop down the screen
+    while (y < maxHeight) {
+        // Loop across columns inside the finish line width
+        for (int col = 0; col < FINISH_LINE_WIDTH; col += STRIPE_WIDTH) {
+            // Alternate colors: depends on row + col
+            uint32_t color;
+            if (((y / STRIPE_HEIGHT) + (col / STRIPE_WIDTH)) % 2 == 0) {
+                color = 0xFFFFFFFF; // white
+            } else {
+                color = 0xFF000000; // black
+            }
+
+            // Draw one checker square
+            drawRectARGB32(
+                x + col,              // x1
+                y,                    // y1
+                x + col + STRIPE_WIDTH,
+                y + STRIPE_HEIGHT,
+                color,
+                1 // filled
+            );
+        }
+
+        y += STRIPE_HEIGHT; // move to next row
+    }
+}
 
    
 
