a724a5bec91fbf1f15a2706a06ebe3b47f162f79
BillyBean <alexandernguyenbill@gmail.com>
2025-09-22 09:59:38 +0700

    - final

--- Task 3/bird/bird_img.h
+++
@@ -1,9 +0,0 @@
-#ifndef BIRD_IMG_H
-#define BIRD_IMG_H
-
-extern const unsigned int bird_mid[];
-extern const unsigned int bird_up[];
-extern const unsigned int bird_down[];
-extern const unsigned int BIRD_PALETTE[6];
-
-#endif
--- Task 3/kernel/timer.h
+++
@@ -1,3 +0,0 @@
-extern unsigned long long cntpct_el0(void);
-extern unsigned int cntfrq_el0(void);
-void wait_msec(unsigned int n);
